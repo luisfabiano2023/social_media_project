1c341b9669431aa97d7ab6240515f5888ec00224
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2024-11-12 13:12:24 -0300

    criando os bagulhos entretanto estou estudando

--- social_media_forum_app/views.py
+++ social_media_forum_app/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from . import models
-from . import serializers
+from . import models as md
+from . import serializers as sz
+from rest_framework import status
 
 @api_view(['GET'])
 def show(request):
@@ -12,11 +13,17 @@ def show(request):
 
 	return Response(show)
 
+
+
 @api_view(['POST'])
 def ct_u(request):
-	# create a new fella on this forum
-
-	return Response(ct_u)
+	users=sz.UserSerializer(data=request.data)
+	if md.User.objects.filter(**request.data).exists():
+		raise sz.serializers.ValidationError("erro,usuario com nome já sendo utilizado,por favor troque as informações ")
+	if users.is_valid():
+		users.save()
+		return Response(users.data)
+	
 
 @api_view(['GET'])
 def dt_u(request):
